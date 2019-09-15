35f1f9a6007c9443f8b2e6058666a0e4340b2dba
Juan Camilo Mojica <juan.mojica@avaldigitallabs.com>
2019-09-14 20:57:08 -0500

    updating example to work with npm and node last versions

--- wdio.conf.js
+++ wdio.conf.js
@@ -58,7 +58,7 @@ exports.config = {
     sync: true,
     //
     // Level of logging verbosity: silent | verbose | command | data | result | error
-    logLevel: 'verbose',
+    logLevel: 'trace',
     //
     // Enables colors for log output.
     coloredLogs: true,
@@ -106,7 +106,10 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
+    runner: 'local',
     services: ['selenium-standalone'],
+    host: '127.0.0.1',
+    port: 4444,
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
